03c4042bff216132203b9e9e322831108da3b221
manlaan <73758520+manlaan@users.noreply.github.com>
2021-06-04 17:21:03 -0600

    .

---
+++ Controls/DrawRing.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Blish_HUD;
+using Blish_HUD.Controls;
+using Blish_HUD.Pathing;
+using Blish_HUD.Pathing.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Blish_HUD.Pathing;
+using Blish_HUD.Pathing.Content;
+using Blish_HUD.Pathing.Format;
+using Blish_HUD.Entities.Primitives;
+
+namespace DistanceRings
+{
+    class DrawRing : Cuboid
+    {
+        public Texture2D RingTexture;
+        public Vector3 RingPosition;
+        public float RingOpacity;
+        public bool RingVisible;
+        public Color RingColor;
+
+        public DrawRing()
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            this.Position = RingPosition;
+            this.Visible = RingVisible;
+            //this.Opacity = RingOpacity;
+        }
+
+        public override void Draw(GraphicsDevice graphicsDevice)
+        {
+            if (_geometryBuffer == null) return;
+
+            _renderEffect.View = GameService.Gw2Mumble.PlayerCamera.View;
+            _renderEffect.Projection = GameService.Gw2Mumble.PlayerCamera.Projection;
+            _renderEffect.World = Matrix.CreateTranslation(_position);
+            _renderEffect.Texture = RingTexture;
+            _renderEffect.Alpha = RingOpacity;
+            _renderEffect.VertexColorEnabled = true;
+
+            /*
+                        _verts[0].Position = new Vector3(-1, 1, -1) * Size;
+                        _verts[0].TextureCoordinate = new Vector2(0, 0);
+                        _verts[1].Position = new Vector3(1, 1, -1) * Size;
+                        _verts[1].TextureCoordinate = new Vector2(1, 0);
+                        _verts[2].Position = new Vector3(-1, 1, 1) * Size;
+                        _verts[2].TextureCoordinate = new Vector2(0, 1);
+                        _verts[3].Position = new Vector3(1, 1, 1) * Size;
+                        _verts[3].TextureCoordinate = new Vector2(1, 1);
+
+                        _verts[4].Position = new Vector3(-1, -1, 1) * Size;
+                        _verts[4].TextureCoordinate = new Vector2(0, 0);
+                        _verts[5].Position = new Vector3(1, -1, 1) * Size;
+                        _verts[5].TextureCoordinate = new Vector2(1, 0);
+                        _verts[6].Position = new Vector3(-1, -1, -1) * Size;
+                        _verts[6].TextureCoordinate = new Vector2(0, 1);
+                        _verts[7].Position = new Vector3(1, -1, -1) * Size;
+                        _verts[7].TextureCoordinate = new Vector2(1, 1);
+
+                        _verts[8].Position = new Vector3(-1, 1, -1) * Size;
+                        _verts[8].TextureCoordinate = new Vector2(0, 0);
+                        _verts[9].Position = new Vector3(-1, 1, 1) * Size;
+                        _verts[9].TextureCoordinate = new Vector2(1, 0);
+                        _verts[10].Position = new Vector3(-1, -1, -1) * Size;
+                        _verts[10].TextureCoordinate = new Vector2(0, 1);
+                        _verts[11].Position = new Vector3(-1, -1, 1) * Size;
+                        _verts[11].TextureCoordinate = new Vector2(1, 1);
+
+                        _verts[12].Position = new Vector3(-1, 1, 1) * Size;
+                        _verts[12].TextureCoordinate = new Vector2(0, 0);
+                        _verts[13].Position = new Vector3(1, 1, 1) * Size;
+                        _verts[13].TextureCoordinate = new Vector2(1, 0);
+                        _verts[14].Position = new Vector3(-1, -1, 1) * Size;
+                        _verts[14].TextureCoordinate = new Vector2(0, 1);
+                        _verts[15].Position = new Vector3(1, -1, 1) * Size;
+                        _verts[15].TextureCoordinate = new Vector2(1, 1);
+
+                        _verts[16].Position = new Vector3(1, 1, 1) * Size;
+                        _verts[16].TextureCoordinate = new Vector2(0, 0);
+                        _verts[17].Position = new Vector3(1, 1, -1) * Size;
+                        _verts[17].TextureCoordinate = new Vector2(1, 0);
+                        _verts[18].Position = new Vector3(1, -1, 1) * Size;
+                        _verts[18].TextureCoordinate = new Vector2(0, 1);
+                        _verts[19].Position = new Vector3(1, -1, -1) * Size;
+                        _verts[19].TextureCoordinate = new Vector2(1, 1);
+
+                        _verts[20].Position = new Vector3(1, 1, -1) * Size;
+                        _verts[20].TextureCoordinate = new Vector2(0, 0);
+                        _verts[21].Position = new Vector3(-1, 1, -1) * Size;
+                        _verts[21].TextureCoordinate = new Vector2(1, 0);
+                        _verts[22].Position = new Vector3(1, -1, -1) * Size;
+                        _verts[22].TextureCoordinate = new Vector2(0, 1);
+                        _verts[23].Position = new Vector3(-1, -1, -1) * Size;
+                        _verts[23].TextureCoordinate = new Vector2(1, 1);
+
+            _geometryBuffer = new VertexBuffer(GameService.Graphics.GraphicsDevice, VertexPositionTexture.VertexDeclaration, 24, BufferUsage.WriteOnly);
+            _geometryBuffer.SetData(_verts);
+
+             */
+
+
+            VertexPositionColorTexture[] v = new VertexPositionColorTexture[4];
+            v[0].Position = new Vector3(-1, 1, 1) * Size;
+            v[0].TextureCoordinate = new Vector2(0, 0);
+            v[0].Color = RingColor;
+            v[1].Position = new Vector3(1, 1, 1) * Size;
+            v[1].TextureCoordinate = new Vector2(1, 0);
+            v[1].Color = RingColor;
+            v[2].Position = new Vector3(-1, -1, 1) * Size;
+            v[2].TextureCoordinate = new Vector2(0, 1);
+            v[2].Color = RingColor;
+            v[3].Position = new Vector3(1, -1, 1) * Size;
+            v[3].TextureCoordinate = new Vector2(1, 1);
+            v[2].Color = RingColor;
+
+            _geometryBuffer = new VertexBuffer(GameService.Graphics.GraphicsDevice, VertexPositionColorTexture.VertexDeclaration, 4, BufferUsage.WriteOnly);
+            _geometryBuffer.SetData(v);
+
+            graphicsDevice.SetVertexBuffer(_geometryBuffer, 0);
+
+            foreach (var pass in _renderEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+            }
+
+            //graphicsDevice.DrawPrimitives(PrimitiveType.TriangleStrip, 0, 23);
+            //graphicsDevice.DrawPrimitives(PrimitiveType.TriangleStrip, 12, 2);
+            graphicsDevice.DrawPrimitives(PrimitiveType.TriangleStrip, 0, 2);         }
+        }
+}
---
+++ Module.cs
@@ -0,0 +1,316 @@
+﻿using Blish_HUD;
+using Blish_HUD.Modules;
+using Blish_HUD.Modules.Managers;
+using Blish_HUD.Settings;
+using Blish_HUD.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+namespace DistanceRings
+{
+    [Export(typeof(Blish_HUD.Modules.Module))]
+    public class DistanceRingsModule : Blish_HUD.Modules.Module
+    {
+
+        private static readonly Logger Logger = Logger.GetLogger<Module>();
+
+        #region Service Managers
+        internal SettingsManager SettingsManager => this.ModuleParameters.SettingsManager;
+        internal ContentsManager ContentsManager => this.ModuleParameters.ContentsManager;
+        internal DirectoriesManager DirectoriesManager => this.ModuleParameters.DirectoriesManager;
+        internal Gw2ApiManager Gw2ApiManager => this.ModuleParameters.Gw2ApiManager;
+        #endregion
+
+        public enum Colors
+        {
+            White,
+            Black,
+            Red,
+            Blue,
+            Green,
+            Cyan,
+            Magenta,
+            Yellow,
+        }
+
+        private SettingEntry<bool> _settingDistanceRingsEnable1;
+        private SettingEntry<bool> _settingDistanceRingsEnable2;
+        private SettingEntry<bool> _settingDistanceRingsEnable3;
+        private SettingEntry<bool> _settingDistanceRingsEnable4;
+        private SettingEntry<bool> _settingDistanceRingsEnable5;
+
+        private SettingEntry<string> _settingDistanceRingsRadius1;
+        private SettingEntry<string> _settingDistanceRingsRadius2;
+        private SettingEntry<string> _settingDistanceRingsRadius3;
+        private SettingEntry<string> _settingDistanceRingsRadius4;
+        private SettingEntry<string> _settingDistanceRingsRadius5;
+
+        private SettingEntry<Colors> _settingDistanceRingsColor1;
+        private SettingEntry<Colors> _settingDistanceRingsColor2;
+        private SettingEntry<Colors> _settingDistanceRingsColor3;
+        private SettingEntry<Colors> _settingDistanceRingsColor4;
+        private SettingEntry<Colors> _settingDistanceRingsColor5;
+
+        private SettingEntry<float> _settingDistanceRingsOpacity1;
+        private SettingEntry<float> _settingDistanceRingsOpacity2;
+        private SettingEntry<float> _settingDistanceRingsOpacity3;
+        private SettingEntry<float> _settingDistanceRingsOpacity4;
+        private SettingEntry<float> _settingDistanceRingsOpacity5;
+
+        private SettingEntry<float> _settingDistanceRingsVerticalOffset;
+
+        private DrawRing _ring1;
+        private DrawRing _ring2;
+        private DrawRing _ring3;
+        private DrawRing _ring4;
+        private DrawRing _ring5;
+
+        private Texture2D _texturesmall;
+        private Texture2D _texturelarge;
+
+        [ImportingConstructor]
+        public DistanceRingsModule([Import("ModuleParameters")] ModuleParameters moduleParameters) : base(moduleParameters) { }
+
+        protected override void DefineSettings(SettingCollection settings)
+        {
+            _settingDistanceRingsEnable1 = settings.DefineSetting("DistanceRingsEnable1", true, "1. Enabled", "");
+            _settingDistanceRingsRadius1 = settings.DefineSetting("DistanceRingsRadius1", "60", "1. Radius", "Radius of the distance ring.");
+            _settingDistanceRingsColor1 = settings.DefineSetting("DistanceRingsColor1", Colors.White, "1. Color", "Color of distance ring.");
+            _settingDistanceRingsOpacity1 = settings.DefineSetting("DistanceRingOpacity1", 100f, "1. Opacity", "Transparency of distance ring.");
+
+            _settingDistanceRingsEnable2 = settings.DefineSetting("DistanceRingsEnable2", false, "2. Enabled", "");
+            _settingDistanceRingsRadius2 = settings.DefineSetting("DistanceRingsRadius2", "90", "2. Radius", "Radius of the distance ring.");
+            _settingDistanceRingsColor2 = settings.DefineSetting("DistanceRingsColor2", Colors.White, "2. Color", "Color of distance ring.");
+            _settingDistanceRingsOpacity2 = settings.DefineSetting("DistanceRingOpacity2", 100f, "2. Opacity", "Transparency of distance ring.");
+
+            _settingDistanceRingsEnable3 = settings.DefineSetting("DistanceRingsEnable3", false, "3. Enabled", "");
+            _settingDistanceRingsRadius3 = settings.DefineSetting("DistanceRingsRadius3", "120", "3. Radius", "Radius of the distance ring.");
+            _settingDistanceRingsColor3 = settings.DefineSetting("DistanceRingsColor3", Colors.White, "3. Color", "Color of distance ring.");
+            _settingDistanceRingsOpacity3 = settings.DefineSetting("DistanceRingOpacity3", 100f, "3. Opacity", "Transparency of distance ring.");
+
+            _settingDistanceRingsEnable4 = settings.DefineSetting("DistanceRingsEnable4", false, "4. Enabled", "");
+            _settingDistanceRingsRadius4 = settings.DefineSetting("DistanceRingsRadius4", "180", "4. Radius", "Radius of the distance ring.");
+            _settingDistanceRingsColor4 = settings.DefineSetting("DistanceRingsColor4", Colors.White, "4. Color", "Color of distance ring.");
+            _settingDistanceRingsOpacity4 = settings.DefineSetting("DistanceRingOpacity4", 100f, "4. Opacity", "Transparency of distance ring.");
+
+            _settingDistanceRingsEnable5 = settings.DefineSetting("DistanceRingsEnable5", false, "5. Enabled", "");
+            _settingDistanceRingsRadius5 = settings.DefineSetting("DistanceRingsRadius5", "1200", "5. Radius", "Radius of the distance ring.");
+            _settingDistanceRingsColor5 = settings.DefineSetting("DistanceRingsColor5", Colors.White, "5. Color", "Color of distance ring.");
+            _settingDistanceRingsOpacity5 = settings.DefineSetting("DistanceRingOpacity5", 100f, "5. Opacity", "Transparency of distance ring.");
+
+            _settingDistanceRingsVerticalOffset = settings.DefineSetting("DistanceRingVerticalOffset", 0f, "Vertical Offset", "How high to offset the distance rings off the ground.");
+
+            _settingDistanceRingsEnable1.SettingChanged += UpdateSettings_Enabled;
+            _settingDistanceRingsEnable2.SettingChanged += UpdateSettings_Enabled;
+            _settingDistanceRingsEnable3.SettingChanged += UpdateSettings_Enabled;
+            _settingDistanceRingsEnable4.SettingChanged += UpdateSettings_Enabled;
+            _settingDistanceRingsEnable5.SettingChanged += UpdateSettings_Enabled;
+            _settingDistanceRingsRadius1.SettingChanged += UpdateSettings_Radius;
+            _settingDistanceRingsRadius2.SettingChanged += UpdateSettings_Radius;
+            _settingDistanceRingsRadius3.SettingChanged += UpdateSettings_Radius;
+            _settingDistanceRingsRadius4.SettingChanged += UpdateSettings_Radius;
+            _settingDistanceRingsRadius5.SettingChanged += UpdateSettings_Radius;
+            _settingDistanceRingsColor1.SettingChanged += UpdateSettings_Color;
+            _settingDistanceRingsColor2.SettingChanged += UpdateSettings_Color;
+            _settingDistanceRingsColor3.SettingChanged += UpdateSettings_Color;
+            _settingDistanceRingsColor4.SettingChanged += UpdateSettings_Color;
+            _settingDistanceRingsColor5.SettingChanged += UpdateSettings_Color;
+            _settingDistanceRingsOpacity1.SettingChanged += UpdateSettings_Opacity;
+            _settingDistanceRingsOpacity2.SettingChanged += UpdateSettings_Opacity;
+            _settingDistanceRingsOpacity3.SettingChanged += UpdateSettings_Opacity;
+            _settingDistanceRingsOpacity4.SettingChanged += UpdateSettings_Opacity;
+            _settingDistanceRingsOpacity5.SettingChanged += UpdateSettings_Opacity;
+        }
+
+        private float DistToPx(float f)
+        {
+            return (float)(f * 2 * .0128);
+        }
+        protected override void Initialize()
+        {
+            _texturesmall = ContentsManager.GetTexture("circle60.png");
+            _texturelarge = ContentsManager.GetTexture("circle500.png");
+
+            _ring1 = new DrawRing();
+            _ring1.RingTexture = _texturesmall;
+            _ring1.Size = new Vector3(10, 10, 0);
+            _ring1.RingOpacity = 1f;
+            _ring1.RingColor = Color.White;
+            _ring1.RingVisible = false;
+            _ring1.Draw(GameService.Graphics.GraphicsDevice);
+            GameService.Graphics.World.Entities.Add(_ring1);
+
+            _ring2 = new DrawRing();
+            _ring2.RingTexture = _texturesmall;
+            _ring2.Size = new Vector3(0, 0, 0);
+            _ring2.RingOpacity = 1f;
+            _ring2.RingColor = Color.White;
+            _ring2.RingVisible = false;
+            _ring2.Draw(GameService.Graphics.GraphicsDevice);
+            GameService.Graphics.World.Entities.Add(_ring2);
+
+            _ring3 = new DrawRing();
+            _ring3.RingTexture = _texturesmall;
+            _ring3.Size = new Vector3(0, 0, 0);
+            _ring3.RingOpacity = 1f;
+            _ring3.RingColor = Color.White;
+            _ring3.RingVisible = false;
+            _ring3.Draw(GameService.Graphics.GraphicsDevice);
+            GameService.Graphics.World.Entities.Add(_ring3);
+
+            _ring4 = new DrawRing();
+            _ring4.RingTexture = _texturesmall;
+            _ring4.Size = new Vector3(0, 0, 0);
+            _ring4.RingOpacity = 1f;
+            _ring4.RingColor = Color.White;
+            _ring4.RingVisible = false;
+            _ring4.Draw(GameService.Graphics.GraphicsDevice);
+            GameService.Graphics.World.Entities.Add(_ring4);
+
+            _ring5 = new DrawRing();
+            _ring5.RingTexture = _texturesmall;
+            _ring5.Size = new Vector3(0, 0, 0);
+            _ring5.RingOpacity = 1f;
+            _ring5.RingColor = Color.White;
+            _ring5.RingVisible = false;
+            _ring5.Draw(GameService.Graphics.GraphicsDevice);
+            GameService.Graphics.World.Entities.Add(_ring5);
+
+            UpdateSettings_Enabled();
+            UpdateSettings_Radius();
+            UpdateSettings_Color();
+            UpdateSettings_Opacity();
+
+        }
+
+        private void UpdateSettings_Enabled(object sender = null, ValueChangedEventArgs<bool> e = null)
+        {
+            _ring1.RingVisible = _settingDistanceRingsEnable1.Value;
+            _ring2.RingVisible = _settingDistanceRingsEnable2.Value;
+            _ring3.RingVisible = _settingDistanceRingsEnable3.Value;
+            _ring4.RingVisible = _settingDistanceRingsEnable4.Value;
+            _ring5.RingVisible = _settingDistanceRingsEnable5.Value;
+        }
+        private void UpdateSettings_Radius(object sender = null, ValueChangedEventArgs<string> e = null)
+        {
+            float diam = 0;
+            diam = DistToPx(float.Parse(_settingDistanceRingsRadius1.Value));
+            _ring1.Size = new Vector3(diam, diam, 0);
+            _ring1.RingTexture = (float.Parse(_settingDistanceRingsRadius1.Value) <= 70 ? _texturesmall : _texturelarge);
+            diam = DistToPx(float.Parse(_settingDistanceRingsRadius2.Value));
+            _ring2.Size = new Vector3(diam, diam, 0);
+            _ring2.RingTexture = (float.Parse(_settingDistanceRingsRadius2.Value) <= 70 ? _texturesmall : _texturelarge);
+            diam = DistToPx(float.Parse(_settingDistanceRingsRadius3.Value));
+            _ring3.Size = new Vector3(diam, diam, 0);
+            _ring3.RingTexture = (float.Parse(_settingDistanceRingsRadius3.Value) <= 70 ? _texturesmall : _texturelarge);
+            diam = DistToPx(float.Parse(_settingDistanceRingsRadius4.Value));
+            _ring4.Size = new Vector3(diam, diam, 0);
+            _ring4.RingTexture = (float.Parse(_settingDistanceRingsRadius4.Value) <= 70 ? _texturesmall : _texturelarge);
+            diam = DistToPx(float.Parse(_settingDistanceRingsRadius5.Value));
+            _ring5.Size = new Vector3(diam, diam, 0);
+            _ring5.RingTexture = (float.Parse(_settingDistanceRingsRadius5.Value) <= 70 ? _texturesmall : _texturelarge);
+        }
+        private void UpdateSettings_Color(object sender = null, ValueChangedEventArgs<Colors> e = null)
+        {
+            _ring1.RingColor = getColor(_settingDistanceRingsColor1.Value);
+            _ring2.RingColor = getColor(_settingDistanceRingsColor2.Value);
+            _ring3.RingColor = getColor(_settingDistanceRingsColor3.Value);
+            _ring4.RingColor = getColor(_settingDistanceRingsColor4.Value);
+            _ring5.RingColor = getColor(_settingDistanceRingsColor5.Value);
+        }
+        private void UpdateSettings_Opacity(object sender = null, ValueChangedEventArgs<float> e = null)
+        {
+            _ring1.RingOpacity = _settingDistanceRingsOpacity1.Value / 100;
+            _ring2.RingOpacity = _settingDistanceRingsOpacity2.Value / 100;
+            _ring3.RingOpacity = _settingDistanceRingsOpacity3.Value / 100;
+            _ring4.RingOpacity = _settingDistanceRingsOpacity4.Value / 100;
+            _ring5.RingOpacity = _settingDistanceRingsOpacity5.Value / 100;
+        }
+
+        protected override async Task LoadAsync()
+        {
+            //return Task.CompletedTask;
+        }
+
+        protected override void OnModuleLoaded(EventArgs e)
+        {
+
+            // Base handler must be called
+            base.OnModuleLoaded(e);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (_settingDistanceRingsEnable1.Value)
+                _ring1.Position = new Vector3(GameService.Gw2Mumble.PlayerCharacter.Position.X, GameService.Gw2Mumble.PlayerCharacter.Position.Y, (GameService.Gw2Mumble.PlayerCharacter.Position.Z + (_settingDistanceRingsVerticalOffset.Value / 20)));
+            if (_settingDistanceRingsEnable2.Value)
+                _ring2.Position = new Vector3(GameService.Gw2Mumble.PlayerCharacter.Position.X, GameService.Gw2Mumble.PlayerCharacter.Position.Y, (GameService.Gw2Mumble.PlayerCharacter.Position.Z + (_settingDistanceRingsVerticalOffset.Value / 20)));
+            if (_settingDistanceRingsEnable3.Value)
+                _ring3.Position = new Vector3(GameService.Gw2Mumble.PlayerCharacter.Position.X, GameService.Gw2Mumble.PlayerCharacter.Position.Y, (GameService.Gw2Mumble.PlayerCharacter.Position.Z + (_settingDistanceRingsVerticalOffset.Value / 20)));
+            if (_settingDistanceRingsEnable4.Value)
+                _ring4.Position = new Vector3(GameService.Gw2Mumble.PlayerCharacter.Position.X, GameService.Gw2Mumble.PlayerCharacter.Position.Y, (GameService.Gw2Mumble.PlayerCharacter.Position.Z + (_settingDistanceRingsVerticalOffset.Value / 20)));
+            if (_settingDistanceRingsEnable5.Value)
+                _ring5.Position = new Vector3(GameService.Gw2Mumble.PlayerCharacter.Position.X, GameService.Gw2Mumble.PlayerCharacter.Position.Y, (GameService.Gw2Mumble.PlayerCharacter.Position.Z + (_settingDistanceRingsVerticalOffset.Value / 20)));
+        }
+
+        /// <inheritdoc />
+        protected override void Unload()
+        {
+            _settingDistanceRingsEnable1.SettingChanged -= UpdateSettings_Enabled;
+            _settingDistanceRingsEnable2.SettingChanged -= UpdateSettings_Enabled;
+            _settingDistanceRingsEnable3.SettingChanged -= UpdateSettings_Enabled;
+            _settingDistanceRingsEnable4.SettingChanged -= UpdateSettings_Enabled;
+            _settingDistanceRingsEnable5.SettingChanged -= UpdateSettings_Enabled;
+            _settingDistanceRingsRadius1.SettingChanged -= UpdateSettings_Radius;
+            _settingDistanceRingsRadius2.SettingChanged -= UpdateSettings_Radius;
+            _settingDistanceRingsRadius3.SettingChanged -= UpdateSettings_Radius;
+            _settingDistanceRingsRadius4.SettingChanged -= UpdateSettings_Radius;
+            _settingDistanceRingsRadius5.SettingChanged -= UpdateSettings_Radius;
+            _settingDistanceRingsColor1.SettingChanged -= UpdateSettings_Color;
+            _settingDistanceRingsColor2.SettingChanged -= UpdateSettings_Color;
+            _settingDistanceRingsColor3.SettingChanged -= UpdateSettings_Color;
+            _settingDistanceRingsColor4.SettingChanged -= UpdateSettings_Color;
+            _settingDistanceRingsColor5.SettingChanged -= UpdateSettings_Color;
+            _settingDistanceRingsOpacity1.SettingChanged -= UpdateSettings_Opacity;
+            _settingDistanceRingsOpacity2.SettingChanged -= UpdateSettings_Opacity;
+            _settingDistanceRingsOpacity3.SettingChanged -= UpdateSettings_Opacity;
+            _settingDistanceRingsOpacity4.SettingChanged -= UpdateSettings_Opacity;
+            _settingDistanceRingsOpacity5.SettingChanged -= UpdateSettings_Opacity;
+
+            GameService.Graphics.World.Entities.Remove(_ring1);
+            GameService.Graphics.World.Entities.Remove(_ring2);
+            GameService.Graphics.World.Entities.Remove(_ring3);
+            GameService.Graphics.World.Entities.Remove(_ring4);
+            GameService.Graphics.World.Entities.Remove(_ring5);
+        }
+
+        private Color getColor(Colors color)
+        {
+            switch (color)
+            {
+                default:
+                case Colors.White:
+                    return Color.White;
+                case Colors.Black:
+                    return Color.Black;
+                case Colors.Red:
+                    return Color.Red;
+                case Colors.Green:
+                    return Color.Green;
+                case Colors.Blue:
+                    return Color.Blue;
+                case Colors.Magenta:
+                    return Color.Magenta;
+                case Colors.Cyan:
+                    return Color.Cyan;
+                case Colors.Yellow:
+                    return Color.Yellow;
+            }
+        }
+
+    }
+
+}
